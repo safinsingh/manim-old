0ce7a6f827c1665f67da4fa9fd06b1da798ac96f
safinsingh235 <42120904+safinsingh235@users.noreply.github.com>
2020-03-27 09:56:08 -0700

    individual mobjects from svg to introtext

--- logo.py
+++ logo.py
@@ -5,7 +5,7 @@ from manimlib.imports import *
 class DrawLogo(Scene):
     def construct(self):
         svg = SVGMobject("logo", fill_opacity=0)
-        introText = TextMobject("Safin Singh")
+        introText = TextMobject("S", "a", "f", "i", "n", " ", "S", "i", "n", "g", "h")
         
         self.play(Write(svg), run_time=4)
         self.play(Transform(svg, introText))
